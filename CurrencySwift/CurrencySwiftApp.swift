8a06844e12c57673fa9941005875f00f290cdc8c
Saydulayev <saydulayev.wien@gmail.com>
2024-06-06 10:28:39 +0200

    Initial commit

---
+++ CurrencySwift/CurrencySwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencySwiftApp.swift
+//  CurrencySwift
+//
+//  Created by Akhmed on 06.06.24.
+//
+
+import SwiftUI
+
+@main
+struct CurrencySwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
